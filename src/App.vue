e26cc1c1731ee64ba954dc13b0c8f1e6a77fe513
veroniquex1 <vdrossouw@gmail.com>
2024-03-18 11:50:32 +0200

    initial nav template and bootstrap installed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar></NavBar>
+  <router-view />
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar
+  }
+}
+
+</script>
 
 <style>
 #app {
