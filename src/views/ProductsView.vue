a7198330d52c801b0a9e9b0f34544e735b3441bd
veroniquex1 <vdrossouw@gmail.com>
2024-03-19 16:12:19 +0200

    table changes

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,14 +1,31 @@
 <template>
-    <h1>Products Page</h1>
-    <TableComp></TableComp>
+    <div class="container px-4 py-5">
+        <h2 class="pb-2 border-bottom d-flex justify-content-center">Products</h2>
+    </div>
+
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                <th scope="col">Edit | Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
-import TableComp from '../components/TableComp.vue'
 
-export default {
-    components: {
-        TableComp
-    }
-}
 </script>
