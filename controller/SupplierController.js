6c120aba5e4c296eabacb2fcde5c353cf0aa417f
veroniquex1 <vdrossouw@gmail.com>
2024-03-20 17:22:01 +0200

    supplier router added

---
+++ controller/SupplierController.js
@@ -0,0 +1,69 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { suppliers } from '../model/index.js'
+
+const supplierRouter = express.Router()
+
+// fetch suppliers
+supplierRouter.get('/', (req, res) => {
+    try {
+        suppliers.fetchSuppliers(req, res)
+    } catch (error) {
+        res.json({
+            status: res.errorCode,
+            message: 'Cannot retrieve supplier lisy. Try again later'
+        })
+    }
+})
+
+// fetch single  supplier 
+supplierRouter.get('/:id', (req, res) => {
+    try {
+        suppliers.fetchSupplier(req, res)
+    } catch (error) {
+        res.json({
+            status: res.errorCode,
+            message: 'Cannot retrieve supplier. Please try again.'
+        })
+    }
+})
+
+//  create new supplier
+supplierRouter.post('/addSupplier', bodyParser.json(), (req, res) => {
+    try {
+        suppliers.addSupplier(req, res)
+    } catch (error) {
+        res.json({
+            status: res.errorCode,
+            message: 'Cannot add supplier.'
+        })
+    }
+})
+
+// update supplier details
+supplierRouter.patch('/update/:id', bodyParser.json(), (req, res) => {
+    try {
+        suppliers.updateSupplier(req, res)
+    } catch (error) {
+        res.json({
+            status: res.errorCode,
+            message: "Cannot update supplier details."
+        })
+    }
+})
+
+// delete supplier
+supplierRouter.delete('/delete/:id', (req, res) => {
+    try {
+        suppliers.deleteSupplier(req, res)
+    } catch (e) {
+        res.json({
+            status: res.errorCode,
+            message: "Could not delete supplier. Try again"
+        })
+    }
+})
+
+export {
+    supplierRouter, express
+}
