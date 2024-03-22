097fc9f84079461a8fee530b09e1626205dbf5b7
veroniquex1 <vdrossouw@gmail.com>
2024-03-22 13:58:45 +0200

    user view and crud added

---
+++ src/components/AddUser.vue
@@ -0,0 +1,91 @@
+<template>
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Add User
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="User ID"
+                                v-model="payload.userID" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="Username"
+                                v-model="payload.username" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="First name"
+                                v-model="payload.firstName" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="Last name"
+                                v-model="payload.lastName" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="Gender"
+                                v-model="payload.gender" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="Role"
+                                v-model="payload.userRole" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="user email"
+                                v-model="payload.userEmail" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 mx-auto" placeholder="user password"
+                                v-model="payload.userPwd" />
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="submit" @click.prevent="addingUser" class="btn btn-success"
+                                data-bs-dismiss="modal">
+                                Add User
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "addUser",
+    data() {
+        return {
+            payload: {
+                userID: null,
+                username: "",
+                firstName: "",
+                lastName: "",
+                gender: "",
+                userRole: "",
+                userEmail: "",
+                userPwd: "",
+            },
+        };
+    },
+    methods: {
+        addingUser() {
+            try {
+                this.$store.dispatch("register", this.payload);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+};
+</script>
+
+<style src="../assets/css/style.css"></style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,15 +1,76 @@
 <template>
-    <h1>Users Page</h1>
-    <TableComp></TableComp>
+    <div class="container px-4 py-5">
+        <h2 class="pb-2 border-bottom d-flex justify-content-center">Manage Users</h2>
+    </div>
 
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">User ID</th>
+                <th scope="col">Username</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">User Role</th>
+
+                <!-- <th scope="col">Total Cost</th> -->
+                <th scope="col">User Email</th>
+                <th scope="col">User Password</th>
+                <th scope="col">Delete | Edit</th>
+            </tr>
+        </thead>
+        <tbody v-if="users">
+            <tr v-for="user in users" :key="user.userID">
+                <td>{{ user.userID }}</td>
+                <td>{{ user.username }}</td>
+                <td>{{ user.firstName }}</td>
+                <td>{{ user.lastName }}</td>
+                <td>{{ user.gender }}</td>
+                <td>{{ user.userRole }}</td>
+                <td>{{ user.userEmail }}</td>
+                <td>{{ user.userPwd }}</td>
+                <td class="d-flex justify-content-between">
+                    <UpdateUser :user="user" />
+                    <button class="btn deleteButton" @click="event => deleteUser(user.userID)"
+                        id="button">Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div>
+        <AddUser class="btn" id="addBtn"></AddUser>
+    </div>
 </template>
 
 <script>
-import TableComp from '../components/TableComp.vue'
 
+import AddUser from '../components/AddUser.vue';
+import UpdateUser from '../components/UpdateUser.vue';
 export default {
     components: {
-        TableComp
+        UpdateUser,
+        AddUser,
+    },
+    computed: {
+        users() {
+            return this.$store.state.users
+        },
+    },
+    mounted() {
+        this.$store.dispatch("fetchUsers")
+    },
+    methods: {
+        deleteUser(userID) {
+            this.$store.dispatch('deleteUser', { id: userID });
+        },
+        UpdateUser(user) {
+            let editUser = { userID: user.userID, username: user.username, firstName: user.firstName, lastName: user.lastName, gender: user.gender, userRole: user.userRole, userEmail: user.userEmail, userPwd: user.userPwd }
+            this.$store.dispatch('UpdateUser', { id: user.userID, data: editUser })
+        }
     }
 }
+
 </script>
+
+<style src="../assets/css/style.css"></style>
