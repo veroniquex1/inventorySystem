0ac8fdd633b09ac84cefc3a2e7d62a4b95aeac7e
veroniquex1 <vdrossouw@gmail.com>
2024-03-22 11:37:50 +0200

    nav changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -31,48 +31,6 @@
             </div>
         </header>
     </div>
-
-
-
-
-    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Twelfth navbar example" id="navbar">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10"
-                aria-controls="navbarsExample10" aria-expanded="true" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a href="/"
-                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                <img src="https://iili.io/JX5AMWg.png" alt="logo">
-            </a>
-
-            <div class="navbar-collapse justify-content-md-center collapse show" id="navbarsExample10" style="">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
-                            aria-expanded="false">Dashboard</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/products">Products</a></li>
-                            <li><a class="dropdown-item" href="/suppliers">Suppliers</a></li>
-                            <li><a class="dropdown-item" href="/users">Users</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="/contact">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="/login">Login</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav> -->
 </template>
 
 <style src="../assets/css/style.css"></style>
